6f24f5d61d62c4b417d67fc26bb682fae071e8c4
avenue17 <daniel080326@gmail.com>
2026-01-16 20:15:50 +0900

    Update tf_idf_search.py

--- tf_idf_search.py
+++ tf_idf_search.py
@@ -1,21 +1,21 @@
-from tf_idf_search import tf_idf_search
-
-documents = [
-    "cats are small animals",
-    "dogs are loyal animals",
-    "cats and dogs can be pets",
-    "cars and bikes are vehicles",
-    "trucks and cars move goods",
-]
-
 def test_tf_idf_search_1():
+    documents = [
+        "cats are small animals",
+        "dogs are loyal animals",
+        "cats and dogs can be pets",
+        "cars and bikes are vehicles",
+        "trucks and cars move goods",
+    ]
     query = "Are cats pets"
-    expected_doc = "cats and dogs can be pets"
-    pred_doc = tf_idf_search(query, documents)
-    assert pred_doc == expected_doc, f"Expected: {expected_doc}, but got: {pred_doc}"
+    assert tf_idf_search(query, documents) == "cats and dogs can be pets"
 
 def test_tf_idf_search_2():
+    documents = [
+        "cats are small animals",
+        "dogs are loyal animals",
+        "cats and dogs can be pets",
+        "cars and bikes are vehicles",
+        "trucks and cars move goods",
+    ]
     query = "Are cars vehicles"
-    expected_doc = "cars and bikes are vehicles"
-    pred_doc = tf_idf_search(query, documents)
-    assert pred_doc == expected_doc, f"Expected: {expected_doc}, but got: {pred_doc}"
+    assert tf_idf_search(query, documents) == "cars and bikes are vehicles"
